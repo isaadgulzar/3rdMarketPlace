8271d2bb021242a830e21737334df296ae64e599
Saad Gulzar <47795736+isaadgulzar@users.noreply.github.com>
2021-02-14 22:00:36 +0500

    Added Shop Page

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// Navbar 
+// For Mobile Navbar 
 function toggleNavOpen() {
     $('#MobileNav').removeClass("hidden");
     $('#closeNavIcon').removeClass("hidden");
@@ -9,3 +9,54 @@ function toggleNavClose() {
     $('#closeNavIcon').addClass('hidden');
     $('#openNavIcon').removeClass("hidden");
 }
+
+// For Responsive Sidebar [Apply Filters]
+$(document).ready(function () {
+    $(window).resize(function() {
+        if ($(this).width() > 1024) {
+            $('#mySidebar').addClass("hidden");
+            $('#mySidebar').removeClass("fixed inset-0 flex z-40");
+            $(".sidebarOverlay").remove();
+            $('#closeSidebarIcon').addClass("hidden");
+            $('#openSidebarIcon').removeClass("hidden");
+        }
+    });
+});
+function openSidebar() {
+    $('#mySidebar').removeClass("hidden ");
+    $('#mySidebar').addClass("fixed inset-0 flex z-40");
+    $("<div class='sidebarOverlay fixed inset-0'><div class='absolute inset-0 bg-gray-600 opacity-75'></div></div>").insertBefore('#actualSidebar');
+    $('#closeSidebarIcon').removeClass("hidden");
+    $('#openSidebarIcon').addClass("hidden");
+}
+function closeSidebar() {
+    $('#mySidebar').addClass("hidden");
+    $('#mySidebar').removeClass("fixed inset-0 flex z-40");
+    $(".sidebarOverlay").remove();
+    $('#closeSidebarIcon').addClass("hidden");
+    $('#openSidebarIcon').removeClass("hidden");
+}
+
+// For Filter Categories Toggle
+$('.open').click(function(e){
+    var el;
+    if(e.target.tagName == 'path') {
+        el = $(e.target).parent();
+    }
+    else {
+        el = $(e.target);
+    }
+    console.log(el);
+    if($(el).hasClass("toggleCross")) {
+        el.removeClass('toggleCross');
+        el.addClass('togglePlus');
+        var elParent = el.parents().closest('.parent');
+        elParent.siblings('.child').addClass('hidden');
+    }
+    else {
+        el.removeClass('togglePlus');
+        el.addClass('toggleCross');
+        var elParent = el.parents().closest('.parent');
+        elParent.siblings('.child').removeClass('hidden');
+    }
+});
